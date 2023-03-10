1724ca2aced6710bc17cc417adea2bab44645bed
Sameer K Muckatira <smuckati@mathworks.com>
2023-03-10 00:00:54 -0800

    Updated test scripts that can be trigggered by CI platform

--- Utilities/scripts_CIAutomation/runProjectTests.m
+++ Utilities/scripts_CIAutomation/runProjectTests.m
@@ -5,26 +5,62 @@
     disp("Project: " + prj.Name)
     disp('Running Model Tests...')
 
-% Run the tests
-    testSuite = sltest.testmanager.load('dcmtrCTRL_PID_Tests.mldatx');
-    result = run(testSuite)
+% Load test file and setup plugins to generate usable test reports
+    testsName = 'dcmtrCTRL_PID_Tests';
+    testSuite = sltest.testmanager.load([testsName '.mldatx']);
     
-% Generate C Code
-    % mdlName = {'dcmtrCtrl_PID','dcmtrCtrl_PID_send2Ardu',...
-    %           'dcmtrCtrl_PID_wHandCode'};
-    % slbuild(mdlName) 
-    % % Saved in currentProject().SimulinkCodeGenFolder
+    import matlab.unittest.TestSuite
+    suite = testsuite([testsName '.mldatx']);
+    
+    import matlab.unittest.TestRunner
+    myrunner = TestRunner.withNoPlugins;
 
-% Save test results
-    disp('Saving test results...')
-    sltest.testmanager.exportResults(result,...
-        fullfile(prj.RootFolder,'GeneratedArtifacts','TestResults','Test Results.mldatx'))
+    % Publish Results as MLDATX
+        import sltest.plugins.TestManagerResultsPlugin
+        mldatxFileName = fullfile(prj.RootFolder,...
+            'GeneratedArtifacts','TestResults',[testsName '_Results.mldatx']);
+        tmr = TestManagerResultsPlugin('ExportToFile',mldatxFileName); 
+        addPlugin(myrunner,tmr)
+    
+    % Publish Results as PDF
+        import matlab.unittest.plugins.TestReportPlugin
+        pdfFile = fullfile(prj.RootFolder,'GeneratedArtifacts','TestResults',[testsName '.pdf']);
+        trp = TestReportPlugin.producingPDF(pdfFile);
+        addPlugin(myrunner,trp)
 
-% Publish testing report
-    sltest.testmanager.report(result, ...
-        fullfile(prj.RootFolder,'GeneratedArtifacts','TestResults','testingReport.zip'),...
-        'IncludeTestResults',0,'IncludeComparisonSignalPlots',true,...
-        'IncludeSimulationSignalPlots',true,'NumPlotRowsPerPage',3);
+    % Publish Results as TAP Report and JUnitFormat
+        import matlab.unittest.plugins.TAPPlugin
+        import matlab.unittest.plugins.XMLPlugin
+        import matlab.unittest.plugins.ToFile
+    
+        tapFile = fullfile(prj.RootFolder,'GeneratedArtifacts','TestResults',[testsName '.tap']);
+        tap = TAPPlugin.producingVersion13(ToFile(tapFile));
+        addPlugin(myrunner,tap)
+        
+        xmlFile = fullfile(prj.RootFolder,'GeneratedArtifacts','TestResults',[testsName '.xml']);
+        p = XMLPlugin.producingJUnitFormat(xmlFile);
+        addPlugin(myrunner,p)
+
+    % Publish Coverage Report
+        import sltest.plugins.coverage.CoverageMetrics
+        cmet = CoverageMetrics('Decision',true,'Condition',true,'MCDC',true);
+    
+        import sltest.plugins.coverage.ModelCoverageReport
+        import matlab.unittest.plugins.codecoverage.CoberturaFormat
+        
+        rptfile = fullfile(prj.RootFolder,'GeneratedArtifacts','TestResults',[testsName '.xml']);
+        rpt = CoberturaFormat(rptfile);
+    
+        import sltest.plugins.ModelCoveragePlugin
+        mcp = ModelCoveragePlugin('Collecting',cmet,'Producing',rpt);
+    
+        addPlugin(myrunner,mcp)
+            % Turn off command line warnings:
+            warning off Stateflow:cdr:VerifyDangerousComparison
+            warning off Stateflow:Runtime:TestVerificationFailed
+
+% Run the tests
+    result = run(myrunner,suite)
 
 % Cleanup
     disp('Tests complete.')
